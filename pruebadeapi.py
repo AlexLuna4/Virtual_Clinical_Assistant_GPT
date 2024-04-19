b49271241557d372ce9e6760ca65405e286c18cd
Alejandro Luna <83666858+AlexLuna4@users.noreply.github.com>
2024-04-18 21:12:47 -0600

    Update pruebadeapi.py

--- pruebadeapi.py
+++ pruebadeapi.py
@@ -7,7 +7,7 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 
 #openai.api_key = os.getenv("OPENAI_API_KEY")
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "sk-00PiTVd2CxO021kMskVT3BlbkFJ0ySNxq0zREInl3wD5r7b" # Generic API
 openai.api_key = OPENAI_API_KEY
 
 print(openai.api_key)
